c9932363d8eec6321944f0cba14cdc9e98f95eb5
atsushi-fukuda <a_fukuda@rococo.co.jp>
2013-06-14 12:23:31 +0900

    end day4 am

--- src/main/typescript/controller/TodoControllerModule.ts
+++ src/main/typescript/controller/TodoControllerModule.ts
@@ -10,25 +10,57 @@ module TodoController {
 
         add : () => void;
         newContent:string;
+        //modContent:string;
+
+        remove:(index:number)=>void;
+        modify:(index:number)=>void;
+
+        debugIndex:number;
 
     }
 
     export class Controller {
-        constructor(public $scope:Scope){
+        constructor(public $scope:Scope,public $window:Window){
             this.$scope.todos = [
                 new Model.Todo("ss1"),
                 new Model.Todo("ss2")
 
             ];
             this.$scope.add = () => this.add();
+            this.$scope.remove = (index:number) => this.remove(index);
+            /*
+            this.$scope.remove = function(index:number):void =>{
+                this.$scope.debugIndex = index;
+                this.$scope.todos.splice(index,1);
+            }
+            */
+
+            this.$scope.modify = (index:number) => this.modify(index);
+            /*
+            this.$scope.modify = function(index:number):void =>{
+                //var targetContent = this.$scope.oldContents[index];
+                //this.$scope.todos[index].content = targetContent;
+            }
+            */
 
         }
 
-        public add(){
+        add(){
             var content:string = this.$scope.newContent;
             var todo:Model.Todo = new Model.Todo(content);
             this.$scope.todos.push(todo);
         }
-
+        remove(index:number){
+            //this.$scope.debugIndex = this.$scope.debugIndex;
+            this.$scope.todos.splice(index,1);
+        }
+        modify(index:number){
+            //this.$scope.modContent = this.$window.prompt("e");
+            var modContent = this.$window.prompt("change to ?",this.$scope.todos[index].content);
+            //console.log("sss" + modContent);
+            if(modContent){
+                this.$scope.todos[index].content = modContent;
+            }
+        }
     }
 }
